ce07463264630e070cbf7c57af117f5120b9fd7d
joeshenouda <shenoudajoseph7@gmail.com>
2026-03-16 14:28:09 -0500

    Set MKL threading layer to GNU in train.py to prevent conflicts with numpy/torch

--- apps/main/train.py
+++ apps/main/train.py
@@ -1,11 +1,14 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
+import os
+# Set before numpy/torch load to avoid MKL vs libgomp conflict
+os.environ.setdefault("MKL_THREADING_LAYER", "GNU")
+
 from copy import deepcopy
 import gc
 import logging
 import math
-import os
 import sys
 import time
 import json
